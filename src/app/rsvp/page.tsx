1e3d06b6ec07df9bd4326c8a47c2b590f531aeec
zfleury <zach@fleury.lol>
2024-08-08 17:09:06 -0500

    its readt

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -4,11 +4,12 @@ const RSVPpage = () => {
   return (
     <>
     <div>RSVPpage</div>
-    <div>You have been invited rsvp pls</div>
+    <div>You have been, invited rsvp pls</div>
     <div className='flex flex-col'>
     <input className='border' placeholder='name' />
     <input className='border' placeholder='song' />
     </div>
+    <a href='https://youtu.be/dQw4w9WgXcQ' className='border-4 bg-slate-300'>submit</a>
     </>
   )
 }
