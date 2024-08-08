28c14584cff2285c51ac4b63824b8aaf0be822e2
zfleury <zach@fleury.lol>
2024-08-08 17:06:41 -0500

    init commit

---
+++ src/app/rsvp/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RSVPpage = () => {
+  return (
+    <>
+    <div>RSVPpage</div>
+    <div>You have been invited rsvp pls</div>
+    <div className='flex flex-col'>
+    <input className='border' placeholder='name' />
+    <input className='border' placeholder='song' />
+    </div>
+    </>
+  )
+}
+
+export default RSVPpage
