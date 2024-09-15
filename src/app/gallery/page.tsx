e7da11215466dddbaa7f56ff962c1fe7d0789911
zfleury <zach@fleury.lol>
2024-09-15 18:50:10 -0500

    moblie testt 2

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -11,12 +11,14 @@ import Image from "next/image";
 
 const Gallery = () => {
   return (
-    <iframe
-      src="https://docs.google.com/presentation/d/1PAsB3CbMwPJxs6B9peV4gw8m2aoWsLHQ2J7gH2P3gXY/embed?start=false&loop=false&delayms=3000"
-      width="400"
-      height="700"
-      allowFullScreen={true}
-    ></iframe>
+    <div className="google-slides-container ">
+      <iframe
+        src="https://docs.google.com/presentation/d/1PAsB3CbMwPJxs6B9peV4gw8m2aoWsLHQ2J7gH2P3gXY/embed?start=false&loop=false&delayms=3000"
+        width="400"
+        height="700"
+        allowFullScreen={true}
+      ></iframe>
+    </div>
   );
 };
 
